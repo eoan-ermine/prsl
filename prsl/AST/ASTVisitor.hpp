e82da5bb6896b949bf9066427e6867d447036660
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-02-02 11:27:30 +0300

    Move unreachable & select to Utils.hpp

--- prsl/AST/ASTVisitor.hpp
+++ prsl/AST/ASTVisitor.hpp
@@ -1,32 +1,15 @@
 #pragma once
 
 #include "prsl/AST/NodeTypes.hpp"
-
-// Не все компиляторы еще поддерживают стандартный unreachable,
-// Так что пусть будет такой
-[[noreturn]] inline void unreachable()
-{
-    // Uses compiler specific extensions if possible.
-    // Even if no extension is used, undefined behavior is still raised by
-    // an empty function body and the noreturn attribute.
-#if defined(_MSC_VER) && !defined(__clang__) // MSVC
-    __assume(false);
-#else // GCC, Clang
-    __builtin_unreachable();
-#endif
-}
+#include "prsl/Utils/Utils.hpp"
 
 namespace prsl::AST {
-
-template<class... Ts>
-struct select : Ts... { using Ts::operator()...; };
-
 
 template <typename ExprVisitRes = void, typename StmtVisitRes = void>
 class ASTVisitor {
 public:
   ExprVisitRes visitExpr(const ExprPtrVariant &expr) {
-    return std::visit<ExprVisitRes>(select{
+    return std::visit<ExprVisitRes>(Utils::select{
       [&](const LiteralExprPtr &expr) { return visitLiteralExpr(expr); },
       [&](const GroupingExprPtr &expr) { return visitGroupingExpr(expr); },
       [&](const VarExprPtr &expr) { return visitVarExpr(expr); },
@@ -41,7 +24,7 @@ public:
     }, expr);
   }
   StmtVisitRes visitStmt(const StmtPtrVariant &stmt) {
-    return std::visit<StmtVisitRes>(select{
+    return std::visit<StmtVisitRes>(Utils::select{
       [&](const VarStmtPtr &stmt) { return visitVarStmt(stmt); },
       [&](const IfStmtPtr &stmt) { return visitIfStmt(stmt); },
       [&](const WhileStmtPtr &stmt) { return visitWhileStmt(stmt); },
---
+++ prsl/Utils/Utils.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace prsl {
+
+namespace Utils {
+
+// Не все компиляторы еще поддерживают стандартный unreachable,
+// Так что пусть будет такой
+[[noreturn]] inline void unreachable() {
+  // Uses compiler specific extensions if possible.
+  // Even if no extension is used, undefined behavior is still raised by
+  // an empty function body and the noreturn attribute.
+#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+  __assume(false);
+#else // GCC, Clang
+  __builtin_unreachable();
+#endif
+}
+
+template <class... Ts> struct select : Ts... {
+  using Ts::operator()...;
+};
+
+} // namespace Utils
+
+} // namespace prsl
