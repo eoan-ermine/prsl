eec260eeaa30f000313f74f36209699fa319c355
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-02-14 22:21:16 +0300

    Implement both clang & gcc CI actions

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -12,3 +12,4 @@ config.test_format = lit.formats.ShTest(True)
 config.test_source_root = os.path.dirname(__file__)
 
 config.substitutions.append(('%edir', lit_config.params["edir"]))
+config.substitutions.append(('%clang', lit_config.params["clang"]))
