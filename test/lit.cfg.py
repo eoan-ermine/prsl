8f89a91942bc9c1a100b396ef699f86f2cf7871f
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-02-22 16:03:05 +0300

    Update tests for new logging format

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -11,5 +11,5 @@ config.test_format = lit.formats.ShTest(True)
 
 config.test_source_root = os.path.dirname(__file__)
 
-config.substitutions.append(('%edir', lit_config.params["edir"]))
+config.substitutions.append(('%edir', "NO_COLOR=1 " + lit_config.params["edir"]))
 config.substitutions.append(('%clang', lit_config.params["clang"]))
